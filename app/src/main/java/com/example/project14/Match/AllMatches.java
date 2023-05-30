2a42b39741db0ce8c6724dd65e62a61fce3576da
J.Seremak <janko2002@outlook.com>
2023-05-30 15:39:56 +0200

    Changed Arrows to buttons

--- app/src/main/java/com/example/project14/Match/AllMatches.java
+++ app/src/main/java/com/example/project14/Match/AllMatches.java
@@ -6,9 +6,11 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
 import android.graphics.ColorSpace;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.project14.R;
@@ -19,6 +21,9 @@ public class AllMatches extends AppCompatActivity {
     private ImageView imgArrowRight;
     private ImageView imgGreenLeft;
     private ImageView imgGreenRight;
+
+    private Button buttonLeft;
+    private Button buttonRight;
     private RecyclerView recyclerView;
 
     @Override
@@ -26,10 +31,14 @@ public class AllMatches extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_matches);
 
-        imgArrowLeft = findViewById(R.id.img_arrow_left);
-        imgArrowRight = findViewById(R.id.img_arrow_right);
-        imgGreenLeft = findViewById(R.id.img_green_left);
-        imgGreenRight = findViewById(R.id.img_green_right);
+//        imgArrowLeft = findViewById(R.id.img_arrow_left);
+//        imgArrowRight = findViewById(R.id.img_arrow_right);
+//        imgGreenLeft = findViewById(R.id.img_green_left);
+//        imgGreenRight = findViewById(R.id.img_green_right);
+
+        buttonLeft = findViewById(R.id.scrollButtonL);
+        buttonRight = findViewById(R.id.scrollButtonR);
+
         recyclerView = findViewById(R.id.recyclerView_matches);
 
         MatchAdapter matchAdapter = new MatchAdapter(this);
@@ -49,29 +58,27 @@ public class AllMatches extends AppCompatActivity {
                 // Check if the RecyclerView can scroll right
                 boolean canScrollRight = recyclerView.canScrollHorizontally(1);
 
-                // Show or hide the left arrow based on the scroll position
-                imgArrowLeft.setVisibility(canScrollLeft ? View.VISIBLE : View.GONE);
-                imgGreenLeft.setVisibility(canScrollLeft ? View.VISIBLE : View.GONE);
+                int grayColor = Color.parseColor("#5A5A5A");
+                int defaultColor = Color.parseColor("#000000");
 
-                // Show or hide the right arrow based on the scroll position
-                imgArrowRight.setVisibility(canScrollRight ? View.VISIBLE : View.GONE);
-                imgGreenRight.setVisibility(canScrollRight ? View.VISIBLE : View.GONE);
+                buttonLeft.setBackgroundColor(canScrollLeft ? defaultColor : grayColor);
+                buttonRight.setBackgroundColor(canScrollRight ? defaultColor : grayColor);
             }
         });
 
-        imgGreenLeft.setOnClickListener(new View.OnClickListener() {
+        buttonLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Scroll the RecyclerView to the left
-                recyclerView.smoothScrollBy(-500, 0);
+                recyclerView.smoothScrollBy(-900, 0);
             }
         });
 
-        imgGreenRight.setOnClickListener(new View.OnClickListener() {
+        buttonRight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Scroll the RecyclerView to the right
-                recyclerView.smoothScrollBy(500, 0);
+                recyclerView.smoothScrollBy(900, 0);
             }
         });
     }
