0a0ef824c4fdcbd13db24bbe68e44ab39f8cd927
J.Seremak <janko2002@outlook.com>
2023-05-30 15:52:15 +0200

    Started working on InsideChats

---
+++ app/src/main/java/com/example/project14/Chats/InsideChat.java
@@ -0,0 +1,14 @@
+package com.example.project14.Chats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsideChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inside_chat);
+    }
+}
