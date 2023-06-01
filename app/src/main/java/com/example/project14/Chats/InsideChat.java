a26799efd605a3eaecb852d116389906522ad159
J.Seremak <janko2002@outlook.com>
2023-06-01 19:26:13 +0200

    Changes regarding Feedback

--- app/src/main/java/com/example/project14/Chats/InsideChat.java
+++ app/src/main/java/com/example/project14/Chats/InsideChat.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.project14.R;
+
 public class InsideChat extends AppCompatActivity {
 
     @Override
