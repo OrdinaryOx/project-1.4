a9aa9d62c5e5bfd8ff562e7e7b7e377ca66787ca
J.Seremak <janko2002@outlook.com>
2023-06-05 14:55:45 +0200

    Added Email and Call functionality
    Dont know if email works since there is no email app on emulator

--- app/src/main/java/com/example/project14/ProfileOther.java
+++ app/src/main/java/com/example/project14/ProfileOther.java
@@ -3,17 +3,26 @@ package com.example.project14;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 public class ProfileOther extends AppCompatActivity {
+
+    private static final int REQUEST_CALL_PHONE_PERMISSION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,7 @@ public class ProfileOther extends AppCompatActivity {
         ImageView backButton = toolbar.findViewById(R.id.back_button);
         ImageView logoButton = toolbar.findViewById(R.id.MWG_logo_IV);
         ImageView optionsButton = toolbar.findViewById(R.id.options_button);
+
         // Set click listener for the back button
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,7 +64,7 @@ public class ProfileOther extends AppCompatActivity {
             }
         });
 
-      //INTENT
+        // INTENT
         Intent intent = getIntent();
         String userName = intent.getStringExtra("username");
         String imageURL = intent.getStringExtra("profileImageURL");
@@ -68,5 +78,54 @@ public class ProfileOther extends AppCompatActivity {
                 .centerCrop()
                 .apply(RequestOptions.bitmapTransform(new CircleCrop()))
                 .into(profileImageIV);
+
+        // EMAIL & CALL
+        ImageView email = findViewById(R.id.imageViewChat);
+        ImageView phone = findViewById(R.id.imageViewCall);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Email Subject");
+                intent.putExtra(Intent.EXTRA_TEXT, "Email Body");
+
+                startActivity(intent);
+
+            }
+        });
+
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(ProfileOther.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ProfileOther.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL_PHONE_PERMISSION);
+                } else {
+                    makePhoneCall();
+                }
+            }
+        });
+    }
+
+    // Handle the permission request result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL_PHONE_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall();
+            } else {
+                Toast.makeText(ProfileOther.this, "Phone call permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Method to initiate the phone call
+    private void makePhoneCall() {
+        String phoneNumber = "0616090398";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        startActivity(intent);
     }
 }
